1faa440e1784344949d314ead2fc920eae4f20a1
yaya <70072204+absolutelyaya@users.noreply.github.com>
2024-10-06 22:42:10 +0200

    added cinematic bars to hud for Unwilling Reverence

--- src/client/java/absolutelyaya/formidulus/mixin/InGameHudMixin.java
+++ src/client/java/absolutelyaya/formidulus/mixin/InGameHudMixin.java
@@ -1,12 +1,14 @@
 package absolutelyaya.formidulus.mixin;
 
 import absolutelyaya.formidulus.gui.TitleHUD;
-import com.llamalad7.mixinextras.sugar.Local;
+import absolutelyaya.formidulus.registries.StatusEffectRegistry;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,9 +16,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class InGameHudMixin
 {
+	@Unique float cinematicBars;
+	
 	@Inject(method="renderTitleAndSubtitle", at = @At(value = "HEAD"))
 	private void init(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci)
 	{
 		TitleHUD.render(context, tickCounter.getLastFrameDuration());
+		
+		if(MinecraftClient.getInstance().player instanceof LivingEntity living && living.hasStatusEffect(StatusEffectRegistry.REVERENCE))
+			cinematicBars = Math.min(cinematicBars + tickCounter.getTickDelta(true) / 20f, 1f);
+		else if(cinematicBars > 0)
+			cinematicBars = Math.max(cinematicBars - tickCounter.getTickDelta(true), 0f);
+		
+		if(cinematicBars > 0f)
+		{
+			context.fill(0, 0, context.getScaledWindowWidth(), (int)(context.getScaledWindowHeight() / 10 * cinematicBars), 0xff000000);
+			int y = context.getScaledWindowHeight();
+			context.fill(0, (int)(y - context.getScaledWindowHeight() / 10 * cinematicBars), context.getScaledWindowWidth(), y, 0xff000000);
+		}
 	}
 }
