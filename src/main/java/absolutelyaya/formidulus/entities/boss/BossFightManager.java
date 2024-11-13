a04cae14c710e6f3ecafa88dd5811ae80c5bc093
yaya <70072204+absolutelyaya@users.noreply.github.com>
2024-11-13 01:30:20 +0100

    removed PersistentBossPosition

--- src/main/java/absolutelyaya/formidulus/entities/boss/BossFightManager.java
+++ src/main/java/absolutelyaya/formidulus/entities/boss/BossFightManager.java
@@ -1,8 +1,6 @@
 package absolutelyaya.formidulus.entities.boss;
 
-import absolutelyaya.formidulus.Formidulus;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
@@ -12,7 +10,6 @@ public class BossFightManager
 	public static final BossFightManager INSTANCE = new BossFightManager();
 	static final Map<UUID, BossFight> activeFights = new HashMap<>();
 	static final Map<BossType, List<UUID>> activeFightIdsByType = new HashMap<>();
-	static final Map<BossType, Map<BlockPos, PersistentBossPosition>> knownPersistentBossPositions = new HashMap<>();
 	static final List<BossFight> endedFights = new ArrayList<>(), newFights = new ArrayList<>();
 	
 	public void tick()
@@ -35,21 +32,6 @@ public class BossFightManager
 		endedFights.clear();
 	}
 	
-	public void registerPersistentBossPosition(PersistentBossPosition ref)
-	{
-		Optional<BossType> prev = getPersistenBossPosTypeIfKnown(ref.pos());
-		if(prev.isPresent())
-		{
-			BlockPos pos = ref.pos();
-			Formidulus.LOGGER.warn("Tried to register Persistent Boss Position of '{}' at [{}, {}, {}], which is already occupied by '{}'",
-					ref.type().id(), pos.getX(), pos.getY(), pos.getZ(), prev.get());
-			return;
-		}
-		if(!knownPersistentBossPositions.containsKey(ref.type()))
-			knownPersistentBossPositions.put(ref.type(), new HashMap<>());
-		knownPersistentBossPositions.get(ref.type()).put(ref.pos(), ref);
-	}
-	
 	public <T extends BossFight> T beginFight(T sender)
 	{
 		if(!newFights.contains(sender))
@@ -61,14 +43,6 @@ public class BossFightManager
 	{
 		if(!endedFights.contains(sender))
 			endedFights.add(sender);
-	}
-	
-	Optional<BossType> getPersistenBossPosTypeIfKnown(BlockPos pos)
-	{
-		for (Map.Entry<BossType, Map<BlockPos, PersistentBossPosition>> type : knownPersistentBossPositions.entrySet())
-			if(type.getValue().containsKey(pos))
-				return Optional.of(type.getKey());
-		return Optional.empty();
 	}
 	
 	public void onPlayerDeath(ServerPlayerEntity player)
--- src/main/java/absolutelyaya/formidulus/entities/boss/PersistentBossPosition.java
+++
@@ -1,7 +0,0 @@
-package absolutelyaya.formidulus.entities.boss;
-
-import net.minecraft.util.math.BlockPos;
-
-public record PersistentBossPosition(BossType type, BlockPos pos)
-{
-}
